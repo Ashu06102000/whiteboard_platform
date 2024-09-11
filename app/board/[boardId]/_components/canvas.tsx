f190574a45242a3ae3a3743c505a327bfdd5700e
yash-copods <yash.thakur@copods.co>
2024-09-12 00:49:38 +0530

    fix

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,9 +1,14 @@
+"use client";
+
+import { useSelf } from "@/liveblocks.config";
 import Info from "./info";
 import Particeipents from "./particepents";
 import Toolbar from "./toolbar";
 import { Canvasprops } from "@/interface/interface";
 
 const Canvas = ({ boardId }: Canvasprops) => {
+  const info = useSelf((me) => me.info);
+  console.log(info, "current_info");
   return (
     <main className="h-full">
       <div className="h-full w-full relative bg-neutral-100 touch-none">
